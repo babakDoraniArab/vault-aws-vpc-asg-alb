bb5915f289fcce5dd6386bac8fa2c148fd9ab8a7
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-11 20:03:40 +0300

    subent edited

--- subnet.tf
+++ subnet.tf
@@ -1,15 +1,13 @@
 resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.1.0/24"
-  #AZ availability zone
+  #AZ availability zone eu-west-1a
   availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name                        = "public-eu-west-1a"
-    "kubernetes.io/cluster/eks" = "shared"
-    "kubernetes.io/role/elb"    = 1
-    Terraform                   = "true"
+    Name      = "public-eu-west-1a"
+    Terraform = "true"
   }
 }
 
@@ -17,15 +15,13 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "private_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.11.0/24"
-  #AZ availability zone
+  #AZ availability zone eu-west-1a
   availability_zone = "eu-west-1a"
 
 
   tags = {
-    Name                              = "private-eu-west-1a"
-    "kubernetes.io/cluster/eks"       = "shared"
-    "kubernetes.io/role/internal-elb" = 1
-    Terraform                         = "true"
+    Name      = "private-eu-west-1a"
+    Terraform = "true"
   }
 }
 
@@ -34,15 +30,13 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "public_2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.2.0/24"
-  #AZ availability zone
+  #AZ availability zone eu-west-1b
   availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name                        = "public-eu-west-1b"
-    "kubernetes.io/cluster/eks" = "shared"
-    "kubernetes.io/role/elb"    = 1
-    Terraform                   = "true"
+    Name      = "public-eu-west-1b"
+    Terraform = "true"
   }
 }
 
@@ -50,15 +44,13 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.12.0/24"
-  #AZ availability zone
+  #AZ availability zone eu-west-1b
   availability_zone = "eu-west-1b"
 
 
   tags = {
-    Name                              = "private-eu-west-1b"
-    "kubernetes.io/cluster/eks"       = "shared"
-    "kubernetes.io/role/internal-elb" = 1
-    Terraform                         = "true"
+    Name      = "private-eu-west-1b"
+    Terraform = "true"
   }
 }
 
