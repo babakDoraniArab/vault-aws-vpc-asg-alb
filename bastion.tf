2bb8034c9b768b059ace127d922be50894ca1687
Babak Doraniarab <babak.dorani@gmail.com>
2022-04-12 12:45:29 +0300

    bastion resource added but not tested yet

---
+++ bastion.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "web" {
+  ami               = "ami-096f7a9ab885b50f4"
+  instance_type     = "t2.micro"
+ 
+  subnet_id         = aws_subnet.public_1.id
+  availability_zone = "eu-west-1a"
+
+
+ 
+
+  security_groups = [aws_security_group.prod_web2.id]
+  key_name        = "Demo2"
+
+
+
+
+  tags = {
+    Name      = "bastion"
+    terraform = "true"
+  }
+}
